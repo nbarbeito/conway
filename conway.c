762e7500acbb689dea9de0e4c7cc589d54b3bbc5
nico nicolas <nico@nicolas.com>
2018-05-05 20:20:32 -0300

    Added TODO

--- conway.c
+++ conway.c
@@ -10,9 +10,6 @@ int main(int argc, char** argv) {
         exit(-1);
 
     CellSpace *cs = create_from_file(argv[1]);
-    //CellSpace* cs = create_random_cells(42, 124);
-    //print_transitions(cs, 100, stdout);
-    //print_transitions_stop_motion(cs);
     show_transitions(cs, 1000000);
     delete_cell_space(cs);
 }
